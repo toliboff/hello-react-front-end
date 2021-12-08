2469d3268c4a6e0fb2f10211ae3b588613ba3b22
Tolibjon <tolibjohn@gmail.com>
2021-12-08 11:26:02 +0500

    Create Greeting component

---
+++ src/components/Greeting.js
@@ -0,0 +1,30 @@
+import React, {useEffect}from "react"
+import { useDispatch, useSelector } from 'react-redux';
+
+import { getGreeting } from '../redux/reducers/greetingReducer';
+
+const Greeting = () => {
+  const message = useSelector((state) => state.message);
+  const dispatch = useDispatch();
+  useEffect(() => {
+    if (!message) {
+      dispatch(getGreeting());
+    }
+  }, []);
+
+  const getAnotherGreeting =() => {
+    dispatch(getGreeting());
+  }
+
+    return (
+      <React.Fragment>
+        <div className='header'>
+          <h1>Greeting of the day</h1>
+          <button onClick={()=>getAnotherGreeting()} className='btn'>Get another greeting</button>
+        </div>
+        <p> { message && message.text }</p>
+      </React.Fragment>
+    );
+}
+
+export default Greeting
